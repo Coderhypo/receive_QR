98de0854fc734b64544a01a772c2e92892ee160b
Coderhypo <i@ihypo.net>
2016-05-11 23:02:52 +0800

    update os

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 
 import os
 
-ALIPAY_URL = os.get('ALIPAY_URL') or 'https://qr.alipay.com/ap5jda2saev94rkof0'
-TENPAY_URL = os.get('TENPAY_URL') or 'https://wx.tenpay.com/f2f?t=AQAAAEK%2F650Wti9EIThvcLSeBYs%3D'
+ALIPAY_URL = os.getenv('ALIPAY_URL') or 'https://qr.alipay.com/ap5jda2saev94rkof0'
+TENPAY_URL = os.getenv('TENPAY_URL') or 'https://wx.tenpay.com/f2f?t=AQAAAEK%2F650Wti9EIThvcLSeBYs%3D'
